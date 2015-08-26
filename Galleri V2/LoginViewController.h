ac2828720b3d763b29b4d2d1f0d02cd7679b3679
Mukul Surajiwale <Mukul@Mukuls-MBP.home>
2015-08-26 04:15:39 -0400

    First commit!

---
+++ Galleri V2/LoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.h
+//  Galleri V2
+//
+//  Created by Mukul Surajiwale on 8/25/15.
+//  Copyright (c) 2015 Mukul Surajiwale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Galleri V2/WelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.h
+//  Galleri V2
+//
+//  Created by Mukul Surajiwale on 8/25/15.
+//  Copyright (c) 2015 Mukul Surajiwale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeViewController : UIViewController
+
+@end
