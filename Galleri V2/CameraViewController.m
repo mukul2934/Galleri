f2724e76c93b1df6a581804cdee9cb2512339c0e
Mukul Surajiwale <Mukul@Mukuls-MBP.home>
2015-08-29 20:58:35 -0400

    Finished up some stuff for the albumView

--- Galleri V2/CameraViewController.m
+++ Galleri V2/CameraViewController.m
@@ -16,15 +16,20 @@
 @property (strong, nonatomic) IBOutlet UILabel *albumLabel;
 @property (strong, nonatomic) IBOutlet UIImageView *previewImageView;
 @property (strong, nonatomic) IBOutlet UIActivityIndicatorView *picUploadActivitySpinner;
-@property (strong, nonatomic) IBOutlet UIVisualEffectView *cameraMenuView;
+
+@property (strong, nonatomic) IBOutlet UIView *cameraMenuView;
 @property (strong, nonatomic) IBOutlet UIVisualEffectView *largeImagePreviewContainerView;
 @property (strong, nonatomic) IBOutlet UIImageView *largeImageView;
+@property (strong, nonatomic) IBOutlet UITextField *albumNameTextField;
+- (IBAction)albumBack:(UISwipeGestureRecognizer *)sender;
 
 #pragma - Actions
 - (IBAction)takePhotoButtonPressed:(UIButton *)sender;
 - (IBAction)swipeGestureShowMenue:(UISwipeGestureRecognizer *)sender;
 - (IBAction)swipeGestureHideMenue:(UISwipeGestureRecognizer *)sender;
 - (IBAction)previewImageButtonPressed:(UIButton *)sender;
+- (IBAction)createNewAlbumButtonPressed:(UIButton *)sender;
+- (IBAction)albumNext:(UISwipeGestureRecognizer *)sender;
 
 #pragma - Properties
 @property (strong, nonatomic) NSMutableArray *albums;
@@ -170,6 +175,88 @@ AVCaptureStillImageOutput *stillImageOutput;
     } else {
         self.largeImagePreviewContainerView.hidden = YES;
         self.previewImageCounter++;
+    }
+}
+
+- (IBAction)createNewAlbumButtonPressed:(UIButton *)sender {
+    
+    PFObject *album = [PFObject objectWithClassName:@"Album"];
+    [album setObject:[PFUser currentUser] forKey:@"Owner"];
+    [album setObject:self.albumNameTextField.text forKey:@"Name"];
+    [album setObject:@"NO" forKey:@"Shared"];
+    [album saveInBackgroundWithBlock:^(BOOL success, NSError *error) {
+        if (success) {
+            PFQuery *query = [PFQuery queryWithClassName:@"Album"];
+            [query whereKey:@"Owner" equalTo:[PFUser currentUser]];
+            [query whereKey:@"Name" equalTo:self.albumNameTextField.text];
+            [query getFirstObjectInBackgroundWithBlock:^(PFObject *object, NSError *error) {
+                if (object) {
+                    PFObject *album = object;
+                    PFObject *albumUserLink = [PFObject objectWithClassName:@"User_Album"];
+                    [albumUserLink setObject:[PFUser currentUser] forKey:@"User"];
+                    [albumUserLink setObject:album forKey:@"Album"];
+                    [albumUserLink saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                        if (succeeded) {
+                            UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Success!" message:@"New album created." delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+                            [alert show];
+                            self.albumLabel.hidden = NO;
+                            self.albumNameTextField.text = @"";
+                            self.currentAlbum = album;
+                            self.albumLabel.text = self.currentAlbum[@"Name"];
+                            [self.albums addObject:album];
+                            self.albumIndex = [self.albums count] - 1;
+                            [self.albumNameTextField resignFirstResponder];
+                        } else {
+                            NSLog(@"Failed to save User <-> Album link");
+                        }
+                    }];
+                } else {
+                    NSLog(@"Failed to retrive album");
+                }
+            }];
+        } else {
+            NSLog(@"%@", error);
+        }
+    }];
+
+}
+
+- (IBAction)albumNext:(UISwipeGestureRecognizer *)sender {
+    NSLog(@"Next Album");
+    if ([self.albums count] > 1 && self.albumIndex != [self.albums count] - 1) {
+        self.albumIndex++;
+        self.currentAlbum = self.albums[self.albumIndex];
+        //self.albumLabel.text = self.currentAlbum[@"Name"];
+        [self.albumLabel setText:self.currentAlbum[@"Name"]];
+        [self.albumLabel performSelector:@selector(setText:) withObject:@"" afterDelay:0.5];
+    } else {
+        self.albumIndex = 0;
+        self.currentAlbum = self.albums[self.albumIndex];
+        [self.albumLabel setText:self.currentAlbum[@"Name"]];
+        [self.albumLabel performSelector:@selector(setText:) withObject:@"" afterDelay:0.5];
+        NSLog(@"Reset albumIdex to 0");
+    }
+}
+
+- (IBAction)albumBack:(UISwipeGestureRecognizer *)sender {
+    NSLog(@"Previous Album");
+    if ([self.albums count] > 1 && self.albumIndex == 0) {
+        int index = [self.albums count];
+        self.albumIndex = index - 1;
+        self.currentAlbum = self.albums[self.albumIndex];
+        [self.albumLabel setText:self.currentAlbum[@"Name"]];
+        [self.albumLabel performSelector:@selector(setText:) withObject:@"" afterDelay:0.5];
+        NSLog(@"Reset albumIndex to %d", self.albumIndex);
+    }else if ([self.albums count] == 1){
+        self.albumIndex = 0;
+        self.currentAlbum = self.albums[self.albumIndex];
+        [self.albumLabel setText:self.currentAlbum[@"Name"]];
+        [self.albumLabel performSelector:@selector(setText:) withObject:@"" afterDelay:0.5];
+    }else{
+        self.albumIndex--;
+        self.currentAlbum = self.albums[self.albumIndex];
+        [self.albumLabel setText:self.currentAlbum[@"Name"]];
+        [self.albumLabel performSelector:@selector(setText:) withObject:@"" afterDelay:0.5];
     }
 }
 @end
